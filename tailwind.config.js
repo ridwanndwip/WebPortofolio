4a4e6ee7f1d3708e33b0084981f8ff89265dff3b
ridwanndwip <ridwanndwip@gmail.com>
2024-09-29 20:40:20 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    screens: {
+      'xs': '480px',
+      's': '576px',
+      'm': '768px',
+      'l': '992px',
+      'xl': '1280px',
+      'hd': '1680px',
+    },
+    extend: {
+      colors: {
+        'cream': '#ECE7E1',
+        'dark' : '#0A0A0A',
+      },
+    },
+  },
+  plugins: [],
+}
